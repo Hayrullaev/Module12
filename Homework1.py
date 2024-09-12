89a05ddb024dc06933a069a663d6a066f85af5cd
Shohruh <shoh0554770876@gmail.com>
2024-09-12 22:00:11 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,29 @@
+
+from runner import Runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_test_walk(self):
+        runner1 = Runner('Shoh')
+        for _ in range(10):
+            runner1.walk()
+        self.assertEqual(runner1.distance, 50)
+
+    def test_test_run(self):
+        runner2 = Runner('Lilya')
+        for _ in range(10):
+            runner2.run()
+        self.assertEqual(runner2.distance, 100)
+
+    def test_test_challenge(self):
+        runner1 = Runner('Shoh')
+        runner2 = Runner('Lilya')
+        for _ in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+    if __name__ == "__main__":
+        unittest.main()
