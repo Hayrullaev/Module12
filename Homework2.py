3ecaf2464c6d23d6e292ce4a9405f971d5d873f7
Shohruh <shoh0554770876@gmail.com>
2024-09-23 14:27:45 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -28,23 +28,7 @@ class TournamentTest(unittest.TestCase):
         TournamentTest.all_results.append(tournament_3.start())
         self.assertTrue(TournamentTest.all_results[-1][3] == self.runner_3)
 
-    def test_tournament_04(self):
-        self.assertTrue(self.prepare_test4() == self.runner_3)
-        with self.assertRaises(Exception):
-            self.prepare_test4()
-
-    def prepare_test4(self):
-        tournament_4 = Tournament(81, self.runner_3, self.runner_2, self.runner_1)
-        TournamentTest.all_results.append(tournament_4.start())
-        last_runner = TournamentTest.all_results[-1][3]
-        second_runner =  TournamentTest.all_results[-1][2]
-        if last_runner != self.runner_3:
-            raise Exception(f'Самый медленный {self.runner_3} должен быть на 3-м месте, а не {last_runner} ')
-        elif second_runner != self.runner_2:
-            raise Exception(f'Вторым должен быть {self.runner_2}, а не {second_runner} ')
-        else:
-            return  self.runner_3
-
+   
     @classmethod
     def tearDownClass(cls):
         for test in cls.all_results:
